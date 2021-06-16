b3d980e06faa64e333e2c92837bcf1da97a8f021
Zero0815 <denny1437@gmail.com>
2021-06-16 11:09:05 +0200

    Update Haushaltsbuch - Kategorie Funktion

--- haushaltsbuch.py
+++ haushaltsbuch.py
@@ -3,6 +3,7 @@
 # -*- coding: utf-8 -*-
 
 # Import der verwendeten Libarys.
+from numpy import number
 import pandas as pd # Zum erstellen der Dataframes und dem einfacheren einlesen der CSV
 import matplotlib.pyplot as plt # Zur erstellung und Ausgabe der Grafiken
 import time # Damit sleep verwendet werden kann
@@ -50,7 +51,7 @@ class Saldo:
             self.start = float(
                 input('Bitte gebe den Startbetrag ein: ').replace(',', '.'))
             with open(out_path, 'w') as file:
-                file.write(f'Art,Betrag,Datum,Saldo\nStartbetrag,{self.start:.2f},{corrected_date},{self.start}')
+                file.write(f'Art,Betrag,Datum,Saldo,Kategorie\nStartbetrag,{self.start:.2f},{corrected_date},{self.start}')
             print(f'Dein Saldo betraegt: {self.start:.2f} €'.replace('.', ','))
             self.current = self.start
             time.sleep(5)
@@ -58,10 +59,37 @@ class Saldo:
     # Funktion zum eintragen einer Einnahme.
     def einnahme(self, betrag):
         
+        if os.path.isfile(category_path):
+            with open(category_path) as file:
+                for line in file:
+                    stripped = line.strip()
+                    category_list.add(stripped)
+        
         # Oeffnet die Output.csv im Erweiterungsmodus und trägt die Einnahme ein, sowie den neu errechneten Saldo.
         with open(out_path, 'a') as file:
             einnahme_betrag = self.current + betrag
-            file.write(f'\nEinnahme,{betrag:.2f},{corrected_date},{einnahme_betrag:.2f}')
+            if len(category_list) == 0:
+                file.write(f'\nEinnahme,{betrag:.2f},{corrected_date},{einnahme_betrag:.2f}')
+            else:
+                corrected_list = []
+                for element in sorted(category_list):
+                    if element in corrected_list:
+                        pass
+                    corrected_list.append(element)
+                ausgabe_liste = []
+                auswahl_ziffer = [(i + 1) for i in range(len(corrected_list))]
+                index_nummer = 0
+                for element in corrected_list:
+                    if element in ausgabe_liste:
+                        pass
+                    ausgabe_liste.append(f'{auswahl_ziffer[index_nummer]}: {element}')
+                    index_nummer += 1
+                print(ausgabe_liste)
+                time.sleep(3)
+                kategorie_wahl = input(f'Bitte wähle die Kategorie aus (1 - {len(category_list)}): ')
+                auswahl = int(kategorie_wahl) - 1
+                kategorie = corrected_list[auswahl]
+                file.write(f'\nEinnahme,{betrag:.2f},{corrected_date},{einnahme_betrag:.2f},{kategorie}')
         df = pd.read_csv(out_path, error_bad_lines=False)
         saldo_new = df['Saldo'].iloc[-1]
         print(f'Dein neuer Saldo beträgt {saldo_new:.2f}€'.replace('.', ','))
@@ -108,6 +136,49 @@ class Saldo:
         plt.ylabel('Datum')
         plt.legend()
         plt.show()
+    
+    #Funktion zum erstellen der Kategorien       
+    def kategorien(self):
+        if os.path.isfile(category_path):
+            with open(category_path, 'r+') as file:
+                new_category = input('Möchtest du weitere Kategorien hinzufügen?\nJa oder Nein: ')
+                if new_category.lower() == 'ja':
+                    while True:
+                        cat = input('Kategorie eingeben, stop umd die Eingabe zu beenden: ')
+                        if cat.lower() == 'stop':
+                            print('Eingabe wird beendet!')
+                            time.sleep(2)
+                            return False
+                        else:
+                            file.write(f'{cat}\n')
+                            
+        if not os.path.isfile(category_path):
+            print('Es wurden keine Kategorien gefunden!')
+            print('Bitte gebe deine Kategorien ein.')
+            time.sleep(3)
+            with open(category_path, 'w+') as file:
+                while True:
+                    cat = input('Bitte gebe deine Kategorien ein, oder schreibe stop um die Eingabe zu beenden: ')
+                    if cat.lower() == 'stop':
+                        print('Eingabe wird beendet!')
+                        time.sleep(2)
+                        return False
+                    else:
+                        file.write(f'{cat}\n')
+                        
+    # Kategorie Ausgabe
+    def kategorie_ausgabe(self):
+        if not os.path.isfile(category_path):
+            print('Du hast noch keine Kategorien erstellt / die Datei ist noch nicht existent!'
+                  '\nBitte führe zuerst die Kategorienfunktion aus.')
+            time.sleep(3)
+        else:
+            with open(category_path, 'r') as file:
+                for line in file:
+                    stripped = line.strip()
+                    category_list.add(stripped)
+            print(sorted(category_list))
+            time.sleep(5)
     
     # Ausgabe einer Grafik, welche anhand von Machine-Learning eine Vorhersage über die erwarteten Änderungen trifft.    
     def vorhersage(self):
@@ -160,7 +231,7 @@ while True:
                   '\n1. "Einnahme" um eine Einnahme einzutragen\n2. "Ausgabe" um eine Ausgabe einzutragen\n'
                   '3. "Liste" um alle Saldos und Buchungen anzuzeigen\n4. "Grafik" um eine Grafik der Saldoänderungen anzuzeigen\n'
                   '5. "Vorhersage" um eine Grafik der erwarteten Saldoänderung anzuzeigen\n6. "Letzte Buchung" um die letzte Buchung anzuzeigen'
-                  #'\n7. "Kategorien" um Buchungskategorien hinzuzufügen'
+                  '\n7. "Kategorien" um Buchungskategorien hinzuzufügen\n8. "Kategorien Ausgabe" um dir die hinterlegten Kategorien auszugeben'
                   '\nUm das Programm zu beenden gebe Quit ein.')
             
             # Auswahl wird in einer Vairable zwischengespeichert.
@@ -192,6 +263,12 @@ while True:
                 
             elif wahl.lower() == 'letzte buchung' or wahl.lower() == '6':
                 s.letzte_buchung()
+                
+            elif wahl.lower() == 'kategorien' or wahl.lower() == '7':
+                s.kategorien()
+                
+            elif wahl.lower() == 'kategorien ausgabe' or wahl.lower() == '8':
+                s.kategorie_ausgabe()
             
             # Sollte keine der Usereingaben erkannt werden, wird die Meldung ausgegeben.
             else:
